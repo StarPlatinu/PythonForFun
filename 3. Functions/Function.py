6df3ab00014ccebeccc7f289904efbded1e3184b
StarPlatinu <thanhvp1509@gmail.com>
2023-05-09 10:07:01 +0700

    function

---
+++ 3. Functions/Function.py
@@ -0,0 +1,19 @@
+#Function in python => def
+
+def thing():
+    print('Hello')
+    print('Fun')
+thing()
+print('zip')
+thing()
+#2 kind of function => Built-in functions / Ffunction we defind
+big = max('Hello world')
+print(big)
+tiny = min('Hello world')
+print(tiny)
+
+#Parameter 
+def greet():return "Hello"
+
+print(greet()," Glenn")
+print(greet()," Sally")
